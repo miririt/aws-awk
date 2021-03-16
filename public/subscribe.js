71785dafdc3e0aabd712d37660808420588678b2
miririt <eliotthescarlet@gmail.com>
2021-03-16 22:43:50 +0900

    친절한 설명 추가

--- public/subscribe.js
+++ public/subscribe.js
@@ -3,9 +3,11 @@ function sendForm(fetchUrl) {
   const subscribeForm = document.querySelector('form');
   const formData = new FormData(subscribeForm);
 
-  fetch(fetchUrl, {
+  return fetch(fetchUrl, {
     method: 'POST',
     body: formData
+  }).catch(err => {
+    alert(`적용 실패===\n${err.stack}`);
   });
 }
 
@@ -14,10 +16,16 @@ window.addEventListener('load', () => {
   const unsubscribeBtn = document.querySelector('#unsubscribe');
 
   subscribeBtn.addEventListener('click', () => {
-    fetchUrl('/subscribe');
+    fetchUrl('/subscribe')
+      .then(() => {
+        alert('구독 성공');
+      });
   });
 
   unsubscribeBtn.addEventListener('click', () => {
-    fetchUrl('/unsubscribe');
+    fetchUrl('/unsubscribe')
+      .then(() => {
+        alert('구독 해지 성공');
+      });
   });
 });
